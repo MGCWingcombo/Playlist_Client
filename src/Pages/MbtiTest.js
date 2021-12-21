2dd2131a948ff6c8d6398570f7d0c3cde21caab1
mingyeongmo <mingyeongmp@naver.com>
2021-12-21 18:21:36 +0900

    버튼마다 원하는 value값 가져오기

--- src/Pages/MbtiTest.js
+++ src/Pages/MbtiTest.js
@@ -1,45 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import Btn from './Btn';
 
 const MbtiTest = () => {
 
-    const [mbtis, setMbtis] = useState(null);
-    const [error, setError] = useState(null);
-
-    
-    
-
-    const holder = async () => {
-        try {
-            setError(null);
-            setMbtis(null);
-            await axios.get('https://jsonplaceholder.typicode.com/posts?_start=0&_end=5')
-            .then(res=>{
-                setMbtis(res.data);
-                console.log("성공");  
-            }).catch(err=>{
-                console.log("실패")
-                setError(err)
-                throw err
-            })
-        } catch (e){
-            return <div>에러가 발생했습니다.</div>;
-        }
-    };
     
 
     
 
     return (
         <>
-            <button onClick={holder}>api get id 1,2</button>
-            {mbtis && (
-                <textarea
-                    rows={7}
-                    value={JSON.stringify(mbtis, null, 2)}
-                    readOnly={true}
-                    />
-            )}
+            <Btn id={1} />
+            <Btn id={2} />
+            <Btn id={3} />
+            
+            
             {/* <ul>
             {mbtis.map(mbti => (
                 <li key={mbti.id}>
