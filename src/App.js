0158589f8d01bef70affc670bd3fa6fefee4cd11
mingyeongmo <mingyeongmp@naver.com>
2021-12-24 22:08:27 +0900

    컴포넌트로 props받은후 그 props에 따라 다른 결과값 받아오기

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Home from "./Pages/Home";
 import "./App.css";
 import Addlist from "./Pages/Addlist/Addlist";
 import Endmbti from "./Pages/Endmbti/Endmbti";
+import MBTI_btn from "./Components/Func/MBTI_btn";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/*" element={<Home />} />
+          <Route path="/*" element={<MBTI_btn />} />
           <Route path="Addmbti" element={<Addlist />} />
           <Route path="mbti-test" element={<MbtiTest />} />
           <Route path="Addmbti/Endmbti" element={<Endmbti />} />
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,37 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// import Addlist from "./Addlist/Addlist";
 import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+const Home = ({ id }) => {
   const navigate = useNavigate();
 
   function navigateClick() {
-    navigate("/Addmbti", { state: { id: 1 } });
-  }
-  function navigateClick2() {
-    navigate("/Addmbti", { state: { id: 2 } });
-  }
-  function navigateClick3() {
-    navigate("/Addmbti", { state: { id: 3 } });
-  }
-  function onAir() {
-    console.log("hi");
+    navigate("/Addmbti", { state: { id: id } });
   }
 
   return (
     <div>
-      <h1 onClick={onAir}>Home입니다.</h1>
-
-      <button onClick={navigateClick}>
-        <div>enfp playlist 추가 id 1</div>
-      </button>
-      <button onClick={navigateClick2}>
-        <div>enfp playlist 추가 id 2</div>
-      </button>
-      <div onClick={navigateClick3}>
-        <button>enfp playlist 추가 id 3</button>
-      </div>
+      <button onClick={navigateClick}>enfp playlist 추가 {id}</button>
     </div>
   );
 };
