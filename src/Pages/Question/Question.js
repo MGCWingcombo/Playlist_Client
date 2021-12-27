7e4298d1ee0f42b9da0290e3030e189646b8d7b3
mingyeongmo <mingyeongmp@naver.com>
2021-12-27 15:06:22 +0900

    question page 퍼블리싱

--- src/Pages/Question/Question.js
+++ src/Pages/Question/Question.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as S from "./Style";
-const Question = ({ fontcolor, btncolor }) => {
+const Question = () => {
   return (
     <>
       <S.Content>
@@ -13,12 +13,24 @@ const Question = ({ fontcolor, btncolor }) => {
           오랜만에 찾아온 휴일, 당신이 원하는 휴식 방법은?
         </S.Questions>
 
-        <S.ChoiceBtn fontcolor="#ffffff" btncolor="#6578a7">
-          집에서 혼자만의 시간을 가진다.
-        </S.ChoiceBtn>
-        <S.ChoiceBtn fontcolor="#000000" btncolor="#ffffff">
-          사람들과 만나서 즐거운 시간을 보낸다.
-        </S.ChoiceBtn>
+        <S.Btns>
+          <S.ChoiceBtn
+            className="topbtn"
+            fontcolor="#ffffff"
+            btncolor="#6578a7"
+            bordercolor="#6578a7"
+          >
+            집에서 혼자만의 시간을 가진다.
+          </S.ChoiceBtn>
+          <S.ChoiceBtn
+            className="bottomBtn"
+            fontcolor="#000000"
+            btncolor="#ffffff"
+            bordercolor="#c4c4c4"
+          >
+            사람들과 만나서 즐거운 시간을 보낸다.
+          </S.ChoiceBtn>
+        </S.Btns>
       </S.Content>
     </>
   );
