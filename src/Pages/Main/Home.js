d8c0126547236e3a3fda47c7c4629d864e22a1bc
mingyeongmo <mingyeongmp@naver.com>
2022-03-25 16:07:23 +0900

    main 페이지 컴포넌트 구조 작업 거의 완료

--- src/Pages/Main/Home.js
+++ src/Pages/Main/Home.js
@@ -1,27 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import * as S from "./Style";
 import { useNavigate } from "react-router-dom";
-import Card from "./M_Card/Card";
 
+import RepeatHomeCon from "./M_Card/Home_Component/repeat_Home_con";
 const Home = () => {
   const navigate = useNavigate();
 
   function navigateClick() {
     navigate("/mbti");
   }
-  const cardWord = [
-    { id: 1, word: "지금계절" },
-    { id: 2, word: "봄" },
-    { id: 3, word: "여름" },
-    { id: 4, word: "가을" },
-    { id: 5, word: "겨울" },
-  ];
-
-  const subtitleWord = [
-    { id: 1, word: "계절" },
-    { id: 2, word: "날씨" },
-    { id: 3, word: "기분" },
-  ];
 
   return (
     <>
@@ -35,47 +22,11 @@ const Home = () => {
           </S.Logo>
           <S.Button onClick={navigateClick}>MBTI 검사하기</S.Button>
         </S.Header>
-        <S.Contents>
-          {subtitleWord.map((sub) => {
-            return (
-              <p className="subtitle" key={sub.id}>
-                {sub.word}로 알아보는 플레이리스트
-              </p>
-            );
-          })}
-          <S.Cardlist>
-            {cardWord.map((list) => {
-              return <Card key={list.id} word={list.word} />;
-            })}
-          </S.Cardlist>
-          {/* <p className="subtitle">계절로 알아보는 플레이리스트</p>
-          <S.Cardlist>
-            {cardWord.map((list) => {
-              return <Card key={list.id} word={list.word} />;
-            })}
-          </S.Cardlist>
-          <p className="subtitle">계절로 알아보는 플레이리스트</p>
-          <S.Cardlist>
-            {cardWord.map((list) => {
-              return <Card key={list.id} word={list.word} />;
-            })}
-          </S.Cardlist> */}
-        </S.Contents>
+
+        <RepeatHomeCon />
       </S.Content>
     </>
   );
 };
 
 export default Home;
-
-{
-  /* {[...Array(5).map((i) => {})]} */
-}
-
-{
-  /* <Card word="지금 계절" />
-            <Card word="봄" />
-            <Card word="여름" />
-            <Card word="가을" />
-            <Card word="겨울" /> */
-}
