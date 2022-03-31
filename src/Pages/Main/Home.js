5f28fe3fb98aa6c3acc61e2aee70111b2ba71d34
mingyeongmo <mingyeongmp@naver.com>
2022-03-31 10:23:37 +0900

    버튼에 링크 추가

--- src/Pages/Main/Home.js
+++ src/Pages/Main/Home.js
@@ -2,12 +2,12 @@ import React from "react";
 import * as S from "./Style";
 import { useNavigate } from "react-router-dom";
 import Mob_header from "../../Components/mobile_header/Mob_header";
-
 import RepeatHomeCon from "./M_Card/Home_Component/repeat_Home_con";
+
 const Home = () => {
   const navigate = useNavigate();
 
-  function navigateClick() {
+  function mbtitest() {
     navigate("/mbti");
   }
 
@@ -21,13 +21,13 @@ const Home = () => {
               alt="playlistlogo"
             />
           </S.Logo>
-          <S.Button onClick={navigateClick}>MBTI 검사하기</S.Button>
+          <S.Button onClick={mbtitest}>MBTI 검사하기</S.Button>
         </S.Header>
 
         <Mob_header prevBtn={false} />
         <S.Condition_Contents>
           <p className="subtitle_mbti">MBTI로 알아보는 플레이리스트</p>
-          <S.Btn>
+          <S.Btn onClick={mbtitest}>
             <img src={require("../../assets/mbtiBtn.png")} alt="mbti_button" />
           </S.Btn>
         </S.Condition_Contents>
