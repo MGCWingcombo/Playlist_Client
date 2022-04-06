93ef1d1418d154139c8f19631d76878f57f0ceaa
mingyeongmo <mingyeongmp@naver.com>
2022-04-06 12:08:53 +0900

    헤더 추가 및 스타일

--- src/Pages/Lastpage/Style.js
+++ src/Pages/Lastpage/Style.js
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 import device from "../../styleds/constant";
+export const Rall = styled.div`
+  background: #f9f9f9;
+  height: 100vh;
+  @media ${device.mobile} {
+    height: 300vh;
+  }
+`;
+
+export const Logo = styled.div`
+  width: 220px;
+  height: 65px;
+  img {
+    width: 220px;
+    height: 65px;
+    padding-top: 3vh;
+    padding-left: 5vw;
+  }
+  @media ${device.mobile} {
+    display: none;
+  }
+`;
 
 export const All = styled.div`
   display: flex;
@@ -26,6 +47,26 @@ export const Data = styled.div`
   overflow-x: hidden;
   border-radius: 20px;
   height: 760px;
+  @media ${device.mobile} {
+    display: flex;
+    overflow-x: auto;
+    overflow-y: hidden;
+    width: 1000px;
+    height: auto;
+  }
+`;
+
+export const Ment = styled.p`
+  font-family: "Prompt";
+  font-size: 1.9rem;
+  font-weight: 500;
+  color: #6c6f8c;
+  margin-left: 50px;
+  @media ${device.mobile} {
+    font-size: 1.7rem;
+    font-weight: 700;
+    color: #6578a7;
+  }
 `;
 
 export const Button = styled.button`
@@ -39,16 +80,11 @@ export const Button = styled.button`
   margin-top: 70px;
   margin-right: 30px;
   border-radius: 10px;
-  border: 1.9px solid #6c6f8c;
+  border: 2px solid #6c6f8c;
   cursor: pointer;
-`;
-
-export const Ment = styled.p`
-  font-family: "Prompt";
-  font-size: 1.9rem;
-  font-weight: 500;
-  color: #6c6f8c;
-  margin-left: 50px;
+  @media ${device.mobile} {
+    display: none;
+  }
 `;
 
 export const Btn = styled.button`
@@ -80,5 +116,7 @@ export const Contents = styled.div`
   }
   .force_data {
     display: none;
+  }
+  @media ${device.mobile} {
   }
 `;
