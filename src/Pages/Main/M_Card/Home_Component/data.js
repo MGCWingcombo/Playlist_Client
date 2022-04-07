441037f8f11025afc932b6403c913df44df66e0e
mingyeongmo <mingyeongmp@naver.com>
2022-04-07 21:10:23 +0900

    데이터 요청을 위한 키 벨류값 추가

--- src/Pages/Main/M_Card/Home_Component/data.js
+++ src/Pages/Main/M_Card/Home_Component/data.js
@@ -2,26 +2,31 @@ export const cardWord1 = [
   {
     id: 1,
     word: "지금 계절에",
+    name: "today-season",
     img: require("../../../../assets/HomeCardImg/one.png"),
   },
   {
     id: 2,
     word: "봄에",
+    name: "spring",
     img: require("../../../../assets/HomeCardImg/two.png"),
   },
   {
     id: 3,
     word: "여름에",
+    name: "summer",
     img: require("../../../../assets/HomeCardImg/three.png"),
   },
   {
     id: 4,
     word: "가을에",
+    name: "autumn",
     img: require("../../../../assets/HomeCardImg/four.png"),
   },
   {
     id: 5,
     word: "겨울에",
+    name: "winter",
     img: require("../../../../assets/HomeCardImg/five.png"),
   },
 ];
@@ -30,26 +35,31 @@ export const cardWord2 = [
   {
     id: 6,
     word: "지금 날씨에",
+    name: "today-weather",
     img: require("../../../../assets/HomeCardImg/six.png"),
   },
   {
     id: 7,
     word: "눈 오는 날",
+    name: "snowy",
     img: require("../../../../assets/HomeCardImg/seven.png"),
   },
   {
     id: 8,
     word: "화창한 날",
+    name: "sunny",
     img: require("../../../../assets/HomeCardImg/eight.png"),
   },
   {
     id: 9,
     word: "비오는 날",
+    name: "rainy",
     img: require("../../../../assets/HomeCardImg/nine.png"),
   },
   {
     id: 10,
-    word: "흐린 날",
+    word: "바람 부는 날",
+    name: "windy",
     img: require("../../../../assets/HomeCardImg/ten.png"),
   },
 ];
@@ -58,26 +68,31 @@ export const cardWord3 = [
   {
     id: 11,
     word: "우울할때",
+    name: "depressed",
     img: require("../../../../assets/HomeCardImg/eleven.png"),
   },
   {
     id: 12,
     word: "신날 때",
+    name: "excited",
     img: require("../../../../assets/HomeCardImg/twelev.png"),
   },
   {
     id: 13,
     word: "잔잔하게",
+    name: "calm",
     img: require("../../../../assets/HomeCardImg/thirteen.png"),
   },
   {
     id: 14,
     word: "운전할 때",
+    name: "drive",
     img: require("../../../../assets/HomeCardImg/fourteen.png"),
   },
   {
     id: 15,
     word: "복잡할 때",
+    name: "tangled",
     img: require("../../../../assets/HomeCardImg/fifteen.png"),
   },
 ];
