13b35b0f31b05d4248ccafd94cf7628e748c3a48
mingyeongmo <mingyeongmp@naver.com>
2022-04-17 00:10:09 +0900

    언더바 뺌

---
+++ src/Components/Mobile_header/Mobheader.js
@@ -0,0 +1,33 @@
+import React from "react";
+import * as S from "./Style";
+import { useNavigate } from "react-router-dom";
+
+const Mobheader = ({ prevBtn }) => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <S.Header>
+        {prevBtn && (
+          <S.PrevBtn
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            <img src={require("../../Assets/chevron.png")} alt="chevron" />
+          </S.PrevBtn>
+        )}
+        <S.Logo>
+          <a href="/">
+            <img
+              src={require("../../Assets/playlistlogo.png")}
+              alt="playlistlogo"
+            />
+          </a>
+        </S.Logo>
+      </S.Header>
+    </>
+  );
+};
+
+export default Mobheader;
